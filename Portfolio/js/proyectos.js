51db0d831304501f574cca10b8f5efb8b0b9cb83
Lucas M. Nuñez <lucas_nunez1998@outlook.es>
2023-11-27 15:28:04 -0300

    nueva update
    Formulario de correo

--- Portfolio/js/proyectos.js
+++ Portfolio/js/proyectos.js
@@ -1,5 +1,5 @@
 const proyectos = [
-    {nombre: 'Poyecto Final Javascript en Casa del Futuro', descripcion: 'Proyecto Final Consumiendo API de peliculas con JS y Local Storage. Para Curso de Desarrollador Javascript.', link:''},
+    {nombre: 'Poyecto Final Javascript en Casa del Futuro', descripcion: 'Proyecto Final Consumiendo API de peliculas con JS y Local Storage. Para Curso de Desarrollador Javascript.', link:'https://proyecto-final-js-cdf-movieapp.vercel.app/'},
     {nombre: 'Trabajo Integrador FullStack Java en CodoAcodo.', descripcion: 'Proyecto Integrador con Java, para DB se uso MySQL Workbench y para Frontend, Html, CSS, Javascript y Bootstrap.', link:''}
 ];
 
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
         <div class="book">
             <p class= "book__p">${proyecto.descripcion}</p>
             <button class="moreInfobtn">
-                <a class="link__proyect" href="#" target="_blank">Ver</a>
+                <a class="link__proyect" href="${proyecto.link}" target="_blank">Ver</a>
             </button>
             <div class="cover">
                 <p class="book__p">${proyecto.nombre}</p>
