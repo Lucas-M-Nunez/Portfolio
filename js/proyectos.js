1a271f0412abf3b810535c64395db2de1e6912b1
Lucas-M-Nunez <lucas_nunez1998@outlook.es>
2024-08-14 18:05:32 -0300

    change

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,5 +1,5 @@
 const proyectos = [
-    {nombre: 'Interfaz Integradora sobre el Curso', descripcion: 'Poyecto Final Javascript en Casa del Futuro. Consumiendo API de peliculas con JS y Local Storage. Para Curso de Desarrollador Javascript.', link:'https://proyecto-final-js-cdf-movieapp.vercel.app/'},
+    {nombre: 'Interfaz Integradora sobre el Curso', descripcion: 'Poyecto Final Javascript en Casa del Futuro. Consumiendo API de peliculas con JS y Local Storage. Para Curso de Desarrollador Javascript.', link:'https://proyecto-final-js-cdf-movieapp.vercel.app/' },
     {nombre: 'Trabajo Integrador Java para Backend en Casa del Futuro', descripcion: 'Proyecto Integrador con Java, Carrito de compras utilizando Spring boot y HTML con thymeleaf y Tamagotchi (Link del Repositorio)', link:'https://github.com/Lucas-M-Nunez/proyectoFinalJava-CDF'},
     {nombre: 'Lista de Pedidos', descripcion: 'Pequeño proyecto de Lista de pedidos usada en emprendimiento de venta para evento de Pascuas', link:'https://order-list-delta.vercel.app/'}
 ];
@@ -25,15 +25,3 @@ window.addEventListener('load', () => {
 
     contenedorCartas.innerHTML = data.join('');
 });
-
-
-
-{/* <div class="book">
-            <p class= "book__p">${proyecto.descripcion}</p>
-            <button class="moreInfobtn">
-                <a class="link__proyect" href="${proyecto.link}" target="_blank">Ver</a>
-            </button>
-            <div class="cover">
-                <p class="book__p">${proyecto.nombre}</p>
-            </div>
-        </div> */}
